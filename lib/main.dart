91f60553a1bf4d2bc04c8f5f9e5427a715faf961
cresus-sadok96 <s.developpement2@cresusnet.ovh>
2022-02-26 22:54:35 +0100

    sadok commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'login_page.dart';
+//import 'splash_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Lato',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
